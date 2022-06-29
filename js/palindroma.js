7e8156807c484589009f6656f09974db7e93e4ba
giomen88 <giorgia9288@virgilio.it>
2022-06-29 14:05:58 +0200

    first commit

---
+++ js/palindroma.js
@@ -0,0 +1,12 @@
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+
+// Consigli del giorno
+// 1. Scriviamo sempre in italiano i passaggi che vogliamo fare
+// 2. Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.
+// Domande da farsi quando scriviamo una funzione
+// ?1 - Come la chiamo?
+// ?2 - Ho bisogno di parametri? (se si quali?)
+// ?3 - Devo restituire qualcosa? (se si cosa? di che tipo?)
+// ?4 OPZIONALE:  Qualcosa  potrebbe andare storto? (se si, cosa?)
+
